72eba62c04ab934ce1b49a8a326589c7d4200b73
Lokesh Gyanchandani <lokesh.sgc@gmail.com>
2014-01-08 05:28:22 -0500

    Insertion and Deletion in a LinkedList
    Signed-off-by: Lokesh Gyanchandani <lokesh.sgc@gmail.com>

--- src/LinkedList.h
+++ src/LinkedList.h
@@ -5,6 +5,7 @@ using namespace std;
 
 #include "Node.h"
 #include <iostream>
+#include <stdlib.h>
 
 template <class T>
 class LinkedList
@@ -16,8 +17,8 @@ class LinkedList
 		LinkedList();
 		~LinkedList();
 
-		void insert(T, int);
 		void insertAtBeginning(T);
+		void insertAtIndex(T, int);
 		void insertAfterElement(T, T);
 		void insertAtEnd(T);
 		void deleteFromBeginning();
@@ -47,25 +48,6 @@ LinkedList<T> :: ~LinkedList()
 }
 
 template <class T>
-int LinkedList<T> :: isEmpty()
-{
-	if (head == NULL)
-		return 1;
-	else
-		return 0;
-}
-
-template <class T>
-void LinkedList<T> :: printList()
-{
-	Node<T> *temp = head;
-	while (temp != NULL) {
-		cout << temp->getInfo() << " ";
-		temp = temp->getNext();
-	}
-}
-
-template <class T>
 void LinkedList<T> :: insertAtBeginning(T value)
 {
 	Node<T> *ptr = new Node<T> ();
@@ -73,6 +55,39 @@ void LinkedList<T> :: insertAtBeginning(T value)
 
 	ptr->setNext(head);
 	head = ptr;
+}
+
+template <class T>
+void LinkedList<T> :: insertAtIndex(T value, int position)
+{
+	int length = this->getLength();
+
+	if (position > length + 1 || position < 1)
+	{
+		cout<<"Position out of bounds"<<endl<<"Terminating";
+		return;
+	}
+
+	Node<T> *ptr = new Node<T>();
+	ptr->setInfo(value);
+
+	if (position == 1)
+	{
+		ptr->setNext(head);
+		head = ptr;
+	}
+	else
+	{
+		Node<T> *temp = head;
+		position = position - 2;
+		while(position > 0)
+		{
+			temp = temp->getNext();
+			position--;
+		}
+		ptr->setNext(temp->getNext());
+		temp->setNext(ptr);
+	}
 }
 
 template <class T>
@@ -90,6 +105,94 @@ void LinkedList<T> :: insertAtEnd(T value)
 			temp = temp->getNext();
 		}
 		temp->setNext(ptr);
+	}
+}
+
+template <class T>
+void LinkedList<T> :: deleteFromBeginning()
+{
+	Node<T> *temp = head;
+
+	if (head == NULL)
+		return;
+
+	head = head->getNext();
+	delete temp;
+}
+
+template <class T>
+void LinkedList<T> :: deleteFromEnd()
+{
+	Node<T> *temp;
+	if (head == NULL)
+	{
+		return;
+	}
+	else if (head->getNext() == NULL)
+	{
+		temp = head;
+		head = NULL;
+		delete temp;
+	}
+	else
+	{
+		temp = head;
+		while(temp->getNext()->getNext() != NULL)
+		{
+			temp = temp->getNext();
+		}
+		delete temp->getNext();
+		temp->setNext(NULL);
+	}
+}
+
+template <class T>
+void LinkedList<T> :: deleteElement(T value)
+{
+	Node<T> *temp, *ntemp;
+	if (head == NULL)
+		return;
+	else {
+		temp = NULL;
+		ntemp = head;
+		while (ntemp!= NULL && ntemp->getInfo() != value)
+		{
+			temp = ntemp;
+			ntemp = ntemp->getNext();
+		}
+		if (ntemp != NULL && ntemp->getInfo() == value)
+		{
+			if (ntemp == head)
+			{
+				temp = head;
+				head = head->getNext();
+				delete temp;
+			}
+			else
+			{
+				temp->setNext(ntemp->getNext());
+				delete ntemp;
+			}
+		}
+	}
+}
+
+template <class T>
+int LinkedList<T> :: isEmpty()
+{
+	if (head == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+template <class T>
+void LinkedList<T> :: printList()
+{
+	Node<T> *temp = head;
+	while (temp != NULL) {
+		cout << temp->getInfo() << " ";
+		temp = temp->getNext();
 	}
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,19 @@
 int main()
 {
 	LinkedList<int> *llist = new LinkedList<int>();
-	llist->insertAtBeginning(0);
-	llist->insertAtEnd(1);
+	//llist->insertAtEnd(1);
 	llist->insertAtEnd(5);
+	//llist->insertAtEnd(9);
+	//llist->insertAtEnd(20);
 	llist->printList();
-	cout<<endl;
-	cout<<llist->getLength();
+	cout<<"\n"<<"length: "<<llist->getLength()<<"\n";
+	llist->insertAtIndex(10, 2);
+	cout<<"\n";
+	llist->printList();
+	cout<<"\n\n";
+	llist->deleteElement(10);
+	llist->deleteElement(5);
+	llist->insertAtEnd(2);
+	llist->printList();
 	return 0;
 }
